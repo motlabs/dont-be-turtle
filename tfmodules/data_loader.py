82b679383e3459f176886855c43bf009c9172f93
jwkangmacpro2 <jwkang10@gmail.com>
2018-06-16 12:36:54 +0900

    (add) four files 껍데기

---
+++ tfmodules/data_loader.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+#! /usr/bin/env python
+'''
+    filename: data_loader.py
+    description: this module undertakes the below items:
+        -
+
+    - Author : Junho Lee and Jaewook Kang @ 2018 June
+
+'''
+
+class Dataloader(object):
+
+    def __init__(self):
+
---
+++ tfmodules/preprocessor.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+#! /usr/bin/env python
+'''
+    filename: preprocessor.py
+    description:
+
+    - Author : Junho Lee and Jaewook Kang @ 2018 June
+
+'''
+
+
+class Preprocessor(object):
+
+
+    def __init__(self):
+
