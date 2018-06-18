52702109abef5fbc25935895a013e403e59531db
jwkangmacpro2 <jwkang10@gmail.com>
2018-06-18 12:23:50 +0900

    (add) skeleton code for data_loader.py / preprocessor

--- tfmodules/data_loader.py
+++ tfmodules/data_loader.py
@@ -3,13 +3,142 @@
 '''
     filename: data_loader.py
     description: this module undertakes the below items:
-        -
+        - Easy delivery of data to the trainer module
+        - Easy extraction of input data from image files.
+        - Easy extraction of label from image filename.
+
+    design doc: https://goo.gl/PTBBVe
+    test data set:
+    https://drive.google.com/drive/u/0/folders/18K1-LJ10ABK2TFXtPbLNYa6BI7LsHCUJ
 
     - Author : Junho Lee and Jaewook Kang @ 2018 June
 
 '''
 
-class Dataloader(object):
+import numpy as np
 
-    def __init__(self):
+class DataSet(object):
+    '''
+        A class for dataset including (input, label)
+    '''
+    def __init__(self,input_data=None,label=None,batchsize=1, is_shuffle=False):
+
+
+        self._np_input_data = input_data
+        self._np_label      = label
+
+        self._curr_batch_index   = 0
+        self.batchsize      = batchsize
+        self._is_shuffle    = is_shuffle
+
+
+    def reset(self):
+
+        self._np_input_data     = np.empty(shape=[0,0])
+        self._np_label          = np.empty(shape=[0,0])
+        self._curr_batch_index  = 0
+
+
+    def set_batchsize(self,batch_size):
+        self.batchsize  = batch_size
+
+
+    def set_dataset(self, input_data,label):
+
+        self._np_input_data = input_data
+        self._np_label      = label
+
+
+
+
+    def merge_dataset(self, np_add_dataset):
+
+
+
+
+    def slice_dataset(self, slice_index):
+
+
+
+
+    def get_batch(self):
+
+
+        # This method returns a batch of a dataset by batch index.
+        # ------------------------------
+
+
+        return np_curr_batch
+
+
+
+
+    def shuffle_dataset(self):
+
+        # This method shuffles an entire dataset
+        #-----------------------------------------
+
+
+        return np_shuffled_dataset
+
+
+
+
+class Dataloader(object):
+    '''
+         A class downloading, extracting and spliting raw dataset to prepare
+         numpy data set.
+
+    '''
+    def __init__(self, train_url,test_url,work_dir):
+        self._train_url     = train_url
+        self._test_url      = test_url
+        self._working_dir   = work_dir
+
+        self._np_train_inputdata   = np.empty(shape=[0,0])
+        self._np_valid_inputdata   = np.empty(shape=[0,0])
+        self._np_test_inputdata    = np.empty(shape=[0,0])
+
+        self._np_train_label       = np.empty(shape=[0,0])
+        self._np_valid_label       = np.empty(shape=[0,0])
+        self._np_test_label        = np.empty(shape=[0,0])
+
+        self.train_filename = ''
+        self.test_filename  = ''
+
+        self.trainset   = DataSet()
+        self.validset   = DataSet()
+        self.testset    = DataSet()
+
+
+    def reset(self):
+
+
+    def download_from_url(self, datatype):
+
+
+    def extract_data_from_file(self,datatype):
+
+
+
+    def extract_lable_from_filename(self,datatype):
+
+
+
+    def get_np_data(self,datatype):
+
+
+    def get_np_label(self,datatype):
+
+
+    def merge_external_data(self,np_external_data,datatype):
+
+
+
+
+
+
+
+
+
 
--- tfmodules/preprocessor.py
+++ tfmodules/preprocessor.py
@@ -3,14 +3,52 @@
 '''
     filename: preprocessor.py
     description:
+    design doc: https://goo.gl/PTBBVe
+
 
     - Author : Junho Lee and Jaewook Kang @ 2018 June
 
 '''
 
+import cv2
+import numpy as np
 
-class Preprocessor(object):
+
+def resize_image(self, np_input_images):
 
 
-    def __init__(self):
+# This method resizes the input image arryar, "np_input_images",
+#  to have weight:height = 3:4 ratio.
 
+
+def run_image_augmentation(np_image_array):
+    '''
+    Scale:
+    목적: 사람과 스마트폰의 거리값은 다양할 수 있다는 UX반영
+    연산: 이미지를 확대하고 줄인다. (배율을 정할 필요 있음)
+
+    Rotation:
+    목적: 손목의 움직임에 따라서 다양한 각도로 촬영 될 수 있다는 UX반영
+    연산: 이미지의 각도를 조절한다.
+    -15,  -5, +5, +15 (deg)
+
+    Shifting
+    목적: 인물은 항상 사진의 중앙에 있지 않기 때문에 그 부분을 반영
+    연산: 이미지를 아래 조합으로 linear shift한다
+    Shift 정도는 어느정도로 해야하는가?
+    좌
+    우
+    좌상
+    좌하
+    우상
+    우하
+
+    Flip
+    목적:
+    이미지의 상하반전 : 스마트폰을 거꾸로 거치 할수 있다.
+    이미지의 좌우 반전: 데이터 셋이 2배가 된다.
+    연산: 이미지의 좌우 반전 and 이미지의 상하 반전
+    '''
+
+
+def annotate_skeleton(np_image_array):
