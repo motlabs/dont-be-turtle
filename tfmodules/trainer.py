39df511656a37d5e62c77ba2c42b443120acb984
jwkangmacpro2 <jwkang10@gmail.com>
2018-06-28 17:36:54 +0900

    (fix) license 문구 추가

--- tfmodules/trainer.py
+++ tfmodules/trainer.py
@@ -1,5 +1,17 @@
-#-*- coding: utf-8 -*-
-#! /usr/bin/env python
+# Copyright 2018 Jaewook Kang and JoonHo Lee ({jwkang10, junhoning}@gmail.com)
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===================================================================================
+# -*- coding: utf-8 -*-#! /usr/bin/env python
 '''
     filename: trainer.py
     description: this is a trainer class in the dont-be-turtle project
