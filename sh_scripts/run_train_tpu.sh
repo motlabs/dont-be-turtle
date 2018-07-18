9cb2ddedc7243a725eac34e942eca301b284d14a
jwkangmacpro2 <jwkang10@gmail.com>
2018-07-18 21:59:53 +0900

    (feat) 쓸떄 없는 짓하지 마라구 ;;

--- sh_scripts/run_train_tpu.sh
+++ sh_scripts/run_train_tpu.sh
@@ -1,22 +1,5 @@
 #! /bin/bash
 
-echo            a8888b.
-echo           d888888b.
-echo           8P"YP"Y88
-echo           8|o||o|88
-echo           8'    .88
-echo           8`._.' Y8.
-echo          d/      `8b.
-echo         dP   .    Y8b.
-echo        d8:'  "  `::88b
-echo       d8"         'Y88b
-echo      :8P    '      :888
-echo       8a.   :     _a88P
-echo     ._/"Yaa_:   .| 88P|
-echo jgs  \    YP"    `| 8P  `.
-echo a:f  /     \.___.d|    .'
-echo     `--..__)8888P`._.'
-
 
 echo =============================================
 echo RUN DONBE-TURTLE-MODEL TRAINING  BY TPU
