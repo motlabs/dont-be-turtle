3b681f0efe1c9e966dd116e9aad56d1260ce85b4
jwkangmacpro2 <jwkang10@gmail.com>
2018-07-18 21:22:11 +0900

    (fix) typepo fix

--- sh_scripts/run_train_tpu.sh
+++ sh_scripts/run_train_tpu.sh
@@ -4,7 +4,7 @@ echo RUN RESNET TRAINING  BY TPU
 export MODEL_BUCKET=gs://dontbeturtle_ckpt
 export DATA_BUCKET=gs://pose_dataset_tfrecord/tfrecords
 
-python ~/dont-be-turtle/tfmodules/train_tpu.py\
+python ~/dont-be-turtle/tfmodules/trainer_tpu.py\
 	  --tpu=$USER-tpu \
 	  --data_dir=${DATA_BUCKET}\
 	  --model_dir=${MODEL_BUCKET}
