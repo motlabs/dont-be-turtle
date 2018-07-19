7919673135371468d2c00e54008e007020afbfc5
jwkangmacpro2 <jwkang10@gmail.com>
2018-07-19 17:47:38 +0900

    OS구분 if 문 추가

--- sh_scripts/run_train_tpu.sh
+++ sh_scripts/run_train_tpu.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-OS := $(shell uname)
-OS_X := Darwin
-
 echo =============================================
 echo RUN DONBE-TURTLE-MODEL TRAINING  BY TPU
 echo JAEWOOK KANG JEJU GOOGLE CAMP 2018
@@ -22,20 +19,25 @@ echo "  '------------------------'"
 echo "      ^      (\_/)"
 echo "      '----- (O.o)"
 echo "             (> <)"
+OS="$(uname -s)"
+OS_X="Darwin"
 
-ifeq ($(OS),$(OS_X))
+
+if [ "$OS" == "$OS_X" ]; then
     export MODEL_BUCKET=/Users/jwkangmacpro2/SourceCodes/dont-be-turtle/tfmodules/export/model
     export DATA_BUCKET=/Users/jwkangmacpro2/SourceCodes/dont-be-turtle/dataset/tfrecords
+    export SOURCE=~/SourceCodes/dont-be-turtle/tfmodules/trainer_tpu.py
 else
     export MODEL_BUCKET=gs://dontbeturtle_ckpt
     export DATA_BUCKET=gs://pose_dataset_tfrecord/tfrecords
-endif
+    export SOURCE=~/dont-be-turtle/tfmodules/trainer_tpu.py
+fi
 
 
 echo "MODEL_BUCKET="${MODEL_BUCKET}
 echo "DATA_BUCKET=" ${DATA_BUCKET}
 
-python ~/dont-be-turtle/tfmodules/trainer_tpu.py\
+python ${SOURCE}\
 	  --tpu=$USER-tpu \
 	  --data_dir=${DATA_BUCKET}/testdataset\
 	  --model_dir=${MODEL_BUCKET}
