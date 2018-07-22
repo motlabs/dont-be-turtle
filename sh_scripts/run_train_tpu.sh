3cca98d3a692f58c9a96e8d240b720678025666b
jwkangmacpro2 <jwkang10@gmail.com>
2018-07-23 02:15:25 +0900

    data_dir restored

--- sh_scripts/run_train_tpu.sh
+++ sh_scripts/run_train_tpu.sh
@@ -45,7 +45,7 @@ echo =============================================
 if [ "$OS" == "$OS_X" ]; then
     python ${SOURCE}\
       --use_tpu=False\
-      --data_dir=None\
+      --data_dir=${DATA_BUCKET}\
       --model_dir=${MODEL_BUCKET}
 else
     python ${SOURCE}\
