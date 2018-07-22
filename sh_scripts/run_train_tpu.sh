b96c217bb39fdefde5e5a9ea3ea04a2d890d466d
jwkangmacpro2 <jwkang10@gmail.com>
2018-07-22 11:35:17 +0900

    changing to real dataset

--- sh_scripts/run_train_tpu.sh
+++ sh_scripts/run_train_tpu.sh
@@ -26,11 +26,11 @@ echo ${OS}
 
 if [ "$OS" == "$OS_X" ]; then
     export MODEL_BUCKET=/Users/jwkangmacpro2/SourceCodes/dont-be-turtle/tfmodules/export/model
-    export DATA_BUCKET=/Users/jwkangmacpro2/SourceCodes/dont-be-turtle/dataset/tfrecords
+    export DATA_BUCKET=/Users/jwkangmacpro2/SourceCodes/dont-be-turtle/dataset/tfrecords/realdataset/
     export SOURCE=~/SourceCodes/dont-be-turtle/tfmodules/trainer_tpu.py
 else
     export MODEL_BUCKET=gs://dontbeturtle_ckpt/
-    export DATA_BUCKET=gs://pose_dataset_tfrecord/tfrecords/testdataset/
+    export DATA_BUCKET=gs://pose_dataset_tfrecord/tfrecords/realdataset/
     export SOURCE=~/dont-be-turtle/tfmodules/trainer_tpu.py
 fi
 
