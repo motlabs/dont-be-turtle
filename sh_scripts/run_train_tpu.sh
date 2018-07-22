dca17023a6ba4764ce7c4dd0fcb90b9954cf98ac
jwkangmacpro2 <jwkang10@gmail.com>
2018-07-22 14:19:08 +0900

    modify run_train_tpu.sh

--- sh_scripts/run_train_tpu.sh
+++ sh_scripts/run_train_tpu.sh
@@ -29,6 +29,7 @@ if [ "$OS" == "$OS_X" ]; then
     export DATA_BUCKET=/Users/jwkangmacpro2/SourceCodes/dont-be-turtle/dataset/tfrecords/realdataset/
     export SOURCE=~/SourceCodes/dont-be-turtle/tfmodules/trainer_tpu.py
 else
+    rm -rf /tmp/gcs_filesystem*
     export MODEL_BUCKET=gs://dontbeturtle_ckpt/
     export DATA_BUCKET=gs://pose_dataset_tfrecord/tfrecords/realdataset/
     export SOURCE=~/dont-be-turtle/tfmodules/trainer_tpu.py
