7295faced5580577194c2b12b336fb02cbd48226
jwkangmacpro2 <jwkang10@gmail.com>
2018-07-23 04:37:30 +0900

    typo correct

--- tfmodules/data_loader_tpu.py
+++ tfmodules/data_loader_tpu.py
@@ -415,7 +415,7 @@ class DataSetInput(object):
         null_image = tf.zeros(shape=[int(DEFAULT_HEIGHT), int(DEFAULT_WIDTH), 3],
                               dtype=tf.bfloat16 if self.use_bfloat16
                               else tf.float32)
-        null_label = tf.zeros(shape=[int(DDEFAULT_HG_INOUT_RESOL), int(DEFAULT_HG_INOUT_RESOL), 4],
+        null_label = tf.zeros(shape=[int(DEFAULT_HG_INOUT_RESOL), int(DEFAULT_HG_INOUT_RESOL), 4],
                               dtype=tf.bfloat16 if self.use_bfloat16
                               else tf.float32)
         return (null_image, null_label)
