374245ab5afcb6660445d4862dcccc1d030e7e4e
jwkangmacpro2 <jwkang10@gmail.com>
2018-07-23 23:12:56 +0900

    depth_multiplier : 0.75 --> 0.5

--- tfmodules/model/model_config.py
+++ tfmodules/model/model_config.py
@@ -276,7 +276,7 @@ class ModelConfig(object):
         self.input_width        = int(DEFAULT_INPUT_RESOL)
         self.input_channel_num  = int(DEFAULT_INPUT_CHNUM)
 
-        self.depth_multiplier   = 0.75 # 1.0 0.75 0.5 0.25
+        self.depth_multiplier   = 0.5 # 1.0 0.75 0.5 0.25
         self.resol_multiplier   = 1.0 # 1.0 0.75 0.5 0.25
         self.num_of_hgstacking  = 2
         self.num_of_labels      = 4
