44b59fa79128525f1e101323a6b9d4179d58ac31
jwkangmacpro2 <jwkang10@gmail.com>
2018-07-23 18:47:56 +0900

    add two script files for multi gpu use

---
+++ sh_scripts/install_cudnn_with_cuda_9_0.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# cudnn down link
+wget https://developer.download.nvidia.com/compute/machine-learning/cudnn/secure/v7.1.4/prod/8.0_20180516/Ubuntu16_04-x64/libcudnn7_7.1.4.18-1%2Bcuda8.0_amd64.deb?Pw--DbIkmF9-Ddyj0vm8cwMG3S5qWfKX8BLEkJugh59ewGUUBkYKguaoPrdq3QD-y1W6B37ADNokJzNLsNska3_Ung6zy1xA19fxXGyUGCg4fAddnBSjtCerrmcE1dPCMfy5dh25_CT2zhjQpbOICx_j6lou6qzOufROICsbO-hshxt6RtO4ojxpYAZzLpghsSEKmw7exvCCbBm8ahGCH5N-UileIhdMHiq5tXw4hPNrjadbQjcgsordinal-virtue-208004
+
+sudo cp cuda/lib64/* /usr/local/cuda-9.0/lib64/
+sudo cp cuda/include/* /usr/local/cuda-9.0/include/
+sudo chmod a+r /usr/local/cuda-9.0/lib64/libcudnn*
+sudo chmod a+r /usr/local/cuda-9.0/include/cudnn.h
+
+# CuDNN installation check
+nvcc --version
+cat /proc/driver/nvidia/version
+cat /usr/local/cuda/include/cudnn.h | grep CUDNN_MAJOR -A 2
