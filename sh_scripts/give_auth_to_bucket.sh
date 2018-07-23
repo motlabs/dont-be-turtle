8a9e7294960d3207331588dedafe9b4f3b10a5a1
jwkangmacpro2 <jwkang10@gmail.com>
2018-07-24 03:56:17 +0900

    give auth to bucket correct

--- sh_scripts/give_auth_to_bucket.sh
+++ sh_scripts/give_auth_to_bucket.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 export YOUR_TPU_NAME=jwkangmacpro2-tpu
-export YOUR_SERVICE_NAME=service-283083143712@cloud-tpu.iam.gserviceaccount.com
+export YOUR_SERVICE_NAME=283083143712-compute@developer.gserviceaccount.com
 
 export DATA_BUCKET=gs://pose_dataset_tfrecord
+
 export MODEL_BUCKET=gs://dontbeturtle_ckpt/
 export TENSORBOARD_BUCKET=gs://dontbeturtle_tflogs/
+
+export MODEL_BUCKET3=gs://dontbeturtle_ckpt3/
+export TENSORBOARD_BUCKET3=gs://dontbeturtle_tflogs3/
 
 echo GET AUTHORIZATION OF GCP BUCKET
 
@@ -23,5 +27,12 @@ gsutil acl ch -u ${YOUR_SERVICE_NAME}:WRITER ${MODEL_BUCKET}
 
 gsutil acl ch -u ${YOUR_SERVICE_NAME}:READER ${TENSORBOARD_BUCKET}
 gsutil acl ch -u ${YOUR_SERVICE_NAME}:WRITER ${TENSORBOARD_BUCKET}
+
+
+gsutil acl ch -u ${YOUR_SERVICE_NAME}:READER ${MODEL_BUCKET3}
+gsutil acl ch -u ${YOUR_SERVICE_NAME}:WRITER ${MODEL_BUCKET3}
+
+gsutil acl ch -u ${YOUR_SERVICE_NAME}:READER ${TENSORBOARD_BUCKET3}
+gsutil acl ch -u ${YOUR_SERVICE_NAME}:WRITER ${TENSORBOARD_BUCKET3}
 
 
