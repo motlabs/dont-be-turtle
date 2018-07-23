121bcb949a642f874f66007e1c1648c3dad0768c
jwkangmacpro2 <jwkang10@gmail.com>
2018-07-23 23:24:42 +0900

    [model] reception layer batch norm 을 slim.conv2d안에서 하는 걸로 수정

--- tfmodules/model/reception_layer.py
+++ tfmodules/model/reception_layer.py
@@ -33,40 +33,48 @@ def get_reception_layer(ch_in,
         endpoint_collection = sc.original_name_scope + '_end_points'
         with slim.arg_scope([slim.conv2d,slim.max_pool2d,model_config.normalizer_fn],
                             outputs_collections=endpoint_collection):
-            # 7x7 conv
-            net = slim.conv2d(inputs                = net,
-                              num_outputs           = model_config.num_of_channels_out,
-                              weights_initializer   = model_config.weights_initializer,
-                              weights_regularizer   = model_config.weights_regularizer,
-                              biases_initializer    = model_config.biases_initializer,
-                              kernel_size           = [model_config.kernel_size,model_config.kernel_size],
-                              padding               = 'SAME',
-                              stride                = [2,2],
-                              normalizer_fn         = None,
-                              activation_fn         = None,
-                              scope                 = scope + '_conv7x7_out')
 
-            # batch_norm
-            net = model_config.normalizer_fn(inputs         = net,
-                                             decay          = model_config.batch_norm_decay,
-                                             fused          = model_config.batch_norm_fused,
-                                             is_training    = model_config.is_trainable,
-                                             activation_fn  = model_config.activation_fn,
-                                             scope          = scope +'_conv7x7_batchnorm_out')
+            ######### temporal codes
+            with slim.arg_scope([model_config.normalizer_fn],
+                                decay=model_config.batch_norm_decay,
+                                fused=model_config.batch_norm_fused,
+                                is_training=model_config.is_trainable,
+                                activation_fn=model_config.activation_fn):
+            ################
+                # 7x7 conv
+                net = slim.conv2d(inputs                = net,
+                                  num_outputs           = model_config.num_of_channels_out,
+                                  weights_initializer   = model_config.weights_initializer,
+                                  weights_regularizer   = model_config.weights_regularizer,
+                                  biases_initializer    = model_config.biases_initializer,
+                                  kernel_size           = [model_config.kernel_size,model_config.kernel_size],
+                                  padding               = 'SAME',
+                                  stride                = [2,2],
+                                  normalizer_fn         = model_config.normalizer_fn,
+                                  activation_fn         = None,
+                                  scope                 = scope + '_conv7x7_out')
 
-            # receptive convolutional block
-            net, receptconv_end_points = get_hourglass_conv_module(ch_in        = net,
-                                                                   ch_out_num   = model_config.num_of_channels_out,
-                                                                   stride       = 1,
-                                                                   model_config = model_config.conv_config,
-                                                                   scope        = scope + '_receptconv')
+                # # batch_norm
+                # net = model_config.normalizer_fn(inputs         = net,
+                #                                  decay          = model_config.batch_norm_decay,
+                #                                  fused          = model_config.batch_norm_fused,
+                #                                  is_training    = model_config.is_trainable,
+                #                                  activation_fn  = model_config.activation_fn,
+                #                                  scope          = scope +'_conv7x7_batchnorm_out')
 
-            # max pooling
-            net =  slim.max_pool2d(inputs           =net,
-                                   kernel_size      =[3,3],
-                                   stride           = [2,2],
-                                   padding          ='SAME',
-                                   scope            = scope +'_maxpool3x3_out')
+                # receptive convolutional block
+                net, receptconv_end_points = get_hourglass_conv_module(ch_in        = net,
+                                                                       ch_out_num   = model_config.num_of_channels_out,
+                                                                       stride       = 1,
+                                                                       model_config = model_config.conv_config,
+                                                                       scope        = scope + '_receptconv')
+
+                # max pooling
+                net =  slim.max_pool2d(inputs           =net,
+                                       kernel_size      =[3,3],
+                                       stride           = [2,2],
+                                       padding          ='SAME',
+                                       scope            = scope +'_maxpool3x3_out')
 
         # Convert end_points_collection into a dictionary of end_points.
         end_points = slim.utils.convert_collection_to_dict(
