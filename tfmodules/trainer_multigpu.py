76cd485cea0e132c584bb920d4c5950352f82c7f
jwkangmacpro2 <jwkang10@gmail.com>
2018-08-12 02:45:45 +0900

    try to bugfix

--- tfmodules/trainer_multigpu.py
+++ tfmodules/trainer_multigpu.py
@@ -286,8 +286,7 @@ def model_fn(features,
         tfestimator = tf.estimator.EstimatorSpec(mode        =mode,
                                                  loss        =loss,
                                                  train_op    =train_op,
-                                                 # eval_metric_ops=metric_ops,
-                                                 training_hooks = [extra_summary_hook])
+                                                 training_hooks = extra_summary_hook)
 
     elif mode == tf.estimator.ModeKeys.EVAL:
         metric_ops = metric_fn(labels, logits_out_heatmap, pck_threshold=FLAGS.pck_threshold)
