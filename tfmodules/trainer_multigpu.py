a325191ac050e88bb91d17f19d4a2c659c77be8e
jwkangmacpro2 <jwkang10@gmail.com>
2018-08-12 04:06:41 +0900

    fix from official codes

--- tfmodules/trainer_multigpu.py
+++ tfmodules/trainer_multigpu.py
@@ -362,18 +362,20 @@ def main(unused_argv):
 
 
     # for CPU or GPU use
-    config = tf.ConfigProto(allow_soft_placement=True,
-                            log_device_placement=True)
-    config.gpu_options.allow_growth=True
+    # config = tf.ConfigProto(allow_soft_placement=True)
+    # config.gpu_options.allow_growth=True
 
-    distribution = tf.contrib.distribute.MirroredStrategy(num_gpus=4)
+    distribution = tf.contrib.distribute.MirroredStrategy(["/device:GPU:0",
+                                                           "/device:GPU:1",
+                                                           "/device:GPU:2",
+                                                           "/device:GPU:3"])
 
     run_config = tf.estimator.RunConfig(
                 model_dir                       =FLAGS.model_dir,
                 tf_random_seed                  =None,
                 save_summary_steps              =FLAGS.summary_step,
                 save_checkpoints_steps          =max(600, FLAGS.iterations_per_loop),
-                session_config                  = config,
+                # session_config                  = config,
                 keep_checkpoint_max             =5,
                 keep_checkpoint_every_n_hours   =10000,
                 log_step_count_steps            =FLAGS.log_step_count_steps,
