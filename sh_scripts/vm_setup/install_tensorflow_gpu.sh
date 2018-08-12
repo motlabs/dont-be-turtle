652e49b7493d87702e9ee07f82a8eec7f3405e43
jwkangmacpro2 <jwkang10@gmail.com>
2018-08-13 02:41:24 +0900

    pip install --user pycocotools 추가

--- sh_scripts/vm_setup/install_tensorflow_gpu.sh
+++ sh_scripts/vm_setup/install_tensorflow_gpu.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-pip install --user tensorflow-gpu==1.9
 pip install --user git+https://github.com/wookayin/tensorflow-plot.git@master
 pip install --user tensorpack
-pip install --user  Cython
-pip install -r  ../../requirement.txt --user
+pip install --user Cython
+pip install --user pycocotools
+pip install --user tensorflow-gpu==1.9
 
 python test_tensorflow_install.py
 
