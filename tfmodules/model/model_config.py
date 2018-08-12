b8d807ddb4e0ac9400ebdec8f1d73005078265bf
jwkangmacpro2 <jwkang10@gmail.com>
2018-08-12 11:33:29 +0900

    num of stage 줄여보기

--- tfmodules/model/model_config.py
+++ tfmodules/model/model_config.py
@@ -328,8 +328,8 @@ class ModelConfig(object):
         self.is_hglayer_conv_after_resize       = True
         self.hglayer_invbottle_expansion_rate   = 6.0
         self.num_of_shorcut_invbottleneck_stacking = 4
-        self.hglayer_num_of_stage               = 4
-        self.num_of_hgstacking                  = 2
+        self.hglayer_num_of_stage               = 2
+        self.num_of_hgstacking                  = 1
 
 
         self.hglayer_conv_type          = 'inverted_bottleneck'
