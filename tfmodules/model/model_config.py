69817a86976503f7634ffeaa05d4af8297388a7c
jwkangmacpro2 <jwkang10@gmail.com>
2018-08-12 11:36:33 +0900

    depth_multiplier = 0.5

--- tfmodules/model/model_config.py
+++ tfmodules/model/model_config.py
@@ -317,7 +317,7 @@ class ModelConfig(object):
         self.input_width        = int(DEFAULT_INPUT_RESOL)
         self.input_channel_num  = int(DEFAULT_INPUT_CHNUM)
 
-        self.depth_multiplier   = 0.125 # 1.0 0.75 0.5 0.25
+        self.depth_multiplier   = 0.5 # 1.0 0.75 0.5 0.25
         self.resol_multiplier   = 1.0 # 1.0 0.75 0.5 0.25
         self.num_of_labels      = NUM_OF_KEYPOINTS
 
@@ -328,7 +328,7 @@ class ModelConfig(object):
         self.is_hglayer_conv_after_resize       = True
         self.hglayer_invbottle_expansion_rate   = 6.0
         self.num_of_shorcut_invbottleneck_stacking = 4
-        self.hglayer_num_of_stage               = 2
+        self.hglayer_num_of_stage               = 4
         self.num_of_hgstacking                  = 1
 
 
