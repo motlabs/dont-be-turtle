c96d143f24ff60741ac535c76369fa7a5dcf969e
jwkangmacpro2 <jwkang10@gmail.com>
2018-08-13 03:05:58 +0900

    auth bucket update

--- sh_scripts/vm_setup/give_auth_to_bucket.sh
+++ sh_scripts/vm_setup/give_auth_to_bucket.sh
@@ -8,8 +8,11 @@ export DATA_BUCKET=gs://pose_dataset_tfrecord
 export MODEL_BUCKET=gs://dontbeturtle_ckpt4/
 #export TENSORBOARD_BUCKET=gs://dontbeturtle_tflogs/
 
-export MODEL_BUCKET3=gs://dontbeturtle_ckpt3/
 export MOBEL_BUCKET_MUTIGPU=gs://dontbeturtle_multigpu
+export MOBEL_BUCKET2=gs://bucket_aisolab
+export MODEL_BUCKET3=gs://dontbeturtle_ckpt3/
+export MOBEL_BUCKET4=gs://bucket_yglee
+
 #export TENSORBOARD_BUCKET3=gs://dontbeturtle_tflogs3/
 
 echo GET AUTHORIZATION OF GCP BUCKET
@@ -29,9 +32,15 @@ gsutil acl ch -u ${YOUR_SERVICE_NAME}:WRITER ${MODEL_BUCKET}
 #gsutil acl ch -u ${YOUR_SERVICE_NAME}:READER ${TENSORBOARD_BUCKET}
 #gsutil acl ch -u ${YOUR_SERVICE_NAME}:WRITER ${TENSORBOARD_BUCKET}
 
+gsutil acl ch -u ${YOUR_SERVICE_NAME}:READER ${MODEL_BUCKET2}
+gsutil acl ch -u ${YOUR_SERVICE_NAME}:WRITER ${MODEL_BUCKET2}
+
 
 gsutil acl ch -u ${YOUR_SERVICE_NAME}:READER ${MODEL_BUCKET3}
 gsutil acl ch -u ${YOUR_SERVICE_NAME}:WRITER ${MODEL_BUCKET3}
+
+gsutil acl ch -u ${YOUR_SERVICE_NAME}:READER ${MODEL_BUCKET4}
+gsutil acl ch -u ${YOUR_SERVICE_NAME}:WRITER ${MODEL_BUCKET4}
 
 
 gsutil acl ch -u ${YOUR_SERVICE_NAME}:READER ${MOBEL_BUCKET_MUTIGPU}
