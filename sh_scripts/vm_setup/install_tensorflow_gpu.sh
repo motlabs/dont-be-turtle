7c94ceea5850eb741741d380666cc044181f6903
jwkangmacpro2 <jwkang10@gmail.com>
2018-08-13 03:27:02 +0900

    batch_size =4, depth_multiplier =0.125

--- sh_scripts/vm_setup/install_tensorflow_gpu.sh
+++ sh_scripts/vm_setup/install_tensorflow_gpu.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+pip install --user opencv-python
 pip install --user git+https://github.com/wookayin/tensorflow-plot.git@master
 pip install --user tensorpack
 pip install --user Cython
