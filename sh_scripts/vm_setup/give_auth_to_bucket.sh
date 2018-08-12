bf6156dc35a0ed7f2d84726410f51a00ec99e533
jwkangmacpro2 <jwkang10@gmail.com>
2018-08-13 03:11:12 +0900

    fix typo

--- sh_scripts/vm_setup/give_auth_to_bucket.sh
+++ sh_scripts/vm_setup/give_auth_to_bucket.sh
@@ -8,10 +8,10 @@ export DATA_BUCKET=gs://pose_dataset_tfrecord
 export MODEL_BUCKET=gs://dontbeturtle_ckpt4/
 #export TENSORBOARD_BUCKET=gs://dontbeturtle_tflogs/
 
-export MOBEL_BUCKET_MUTIGPU=gs://dontbeturtle_multigpu
-export MOBEL_BUCKET2=gs://bucket_aisolab
-export MODEL_BUCKET3=gs://dontbeturtle_ckpt3/
-export MOBEL_BUCKET4=gs://bucket_yglee
+export MODEL_BUCKET_MUTIGPU=gs://dontbeturtle_multigpu
+export MODEL_BUCKET2=gs://bucket_aisolab
+export MODEL_BUCKET3=gs://dontbeturtle_ckpt3
+export MODEL_BUCKET4=gs://bucket_yglee
 
 #export TENSORBOARD_BUCKET3=gs://dontbeturtle_tflogs3/
 
@@ -43,8 +43,8 @@ gsutil acl ch -u ${YOUR_SERVICE_NAME}:READER ${MODEL_BUCKET4}
 gsutil acl ch -u ${YOUR_SERVICE_NAME}:WRITER ${MODEL_BUCKET4}
 
 
-gsutil acl ch -u ${YOUR_SERVICE_NAME}:READER ${MOBEL_BUCKET_MUTIGPU}
-gsutil acl ch -u ${YOUR_SERVICE_NAME}:WRITER ${MOBEL_BUCKET_MUTIGPU}
+gsutil acl ch -u ${YOUR_SERVICE_NAME}:READER ${MODEL_BUCKET_MUTIGPU}
+gsutil acl ch -u ${YOUR_SERVICE_NAME}:WRITER ${MODEL_BUCKET_MUTIGPU}
 
 #gsutil acl ch -u ${YOUR_SERVICE_NAME}:READER ${TENSORBOARD_BUCKET3}
 #gsutil acl ch -u ${YOUR_SERVICE_NAME}:WRITER ${TENSORBOARD_BUCKET3}
