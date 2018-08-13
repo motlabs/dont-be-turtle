c8af8b31bfe81c46920848981472728da99ad619
jwkangmacpro2 <jwkang10@gmail.com>
2018-08-13 11:26:55 +0900

    self.hglayer_invbottle_expansion_rate   = 3.0

--- tfmodules/model/model_config.py
+++ tfmodules/model/model_config.py
@@ -326,8 +326,8 @@ class ModelConfig(object):
         # hglayer
         self.is_hglayer_shortcut_conv           = True
         self.is_hglayer_conv_after_resize       = True
-        self.hglayer_invbottle_expansion_rate   = 6.0
-        self.num_of_shorcut_invbottleneck_stacking = 2
+        self.hglayer_invbottle_expansion_rate   = 3.0
+        self.num_of_shorcut_invbottleneck_stacking = 4
         self.hglayer_num_of_stage               = 4
         self.num_of_hgstacking                  = 1
 
