3b3247cee035eb8718d1eedbd294c8b4f1578dc8
jwkangmacpro2 <jwkang10@gmail.com>
2018-08-15 22:09:47 +0900

    self.weights_regularizer= None self.depth_multiplier   = 0.0625 self.is_hglayer_shortcut_conv = True self.is_hglayer_conv_after_resize = True Self.hg_layer_convbottem_type = ‘inverted_bottleneck’ Self.hglayer_invbottle_expansion_rate =7 Self.rclayer_invbottle_expansion_rate =7 num_of_shorcut_invbottleneck_stacking = 4 Self.hglayer_num_of_stage = 4 Self.num_of_hgstacking = 1 Batch_size = 64 Learning rate base  =1-e3

--- tfmodules/model/model_config.py
+++ tfmodules/model/model_config.py
@@ -334,8 +334,8 @@ class ModelConfig(object):
         # hglayer
         self.is_hglayer_shortcut_conv           = True
         self.is_hglayer_conv_after_resize       = True
-        self.hglayer_invbottle_expansion_rate   = 10.0
-        self.rclayer_invbottle_expansion_rate   = 10.0
+        self.hglayer_invbottle_expansion_rate   = 7.0
+        self.rclayer_invbottle_expansion_rate   = 7.0
         self.num_of_shorcut_invbottleneck_stacking = 4
         self.hglayer_num_of_stage               = 4
         self.num_of_hgstacking                  = 1
