8bdc5596ed31c94b8337e3d8914464dfc61eaa07
jwkangmacpro2 <jwkang10@gmail.com>
2018-11-08 21:57:17 +0900

    baseline5 setup

--- tfmodules/model/model_config.py
+++ tfmodules/model/model_config.py
@@ -338,7 +338,7 @@ class ModelConfig(object):
         self.rclayer_invbottle_expansion_rate   = 6.0
         self.num_of_shorcut_invbottleneck_stacking = 4
         self.hglayer_num_of_stage               = 4
-        self.num_of_hgstacking                  = 4
+        self.num_of_hgstacking                  = 1
 
 
         self.hglayer_conv_type          = 'inverted_bottleneck'
