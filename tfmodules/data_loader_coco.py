2c88056f3e0fa60322e16432b84c91bab4d55569
jwkangmacpro2 <jwkang10@gmail.com>
2018-11-22 05:20:23 +0900

    변화하는 api에 맞춰서 수정

--- tfmodules/data_loader_coco.py
+++ tfmodules/data_loader_coco.py
@@ -216,8 +216,7 @@ class DataSetInput(object):
                 )
             ), num_parallel_calls=multiprocessing_num)
 
-        dataset = dataset.batch(batch_size=batch_size,
-                                drop_remainder=True)
+        dataset = dataset.batch_and_drop_remainder(batch_size=batch_size)
         dataset = dataset.map(functools.partial(self._set_shapes, batch_size),
                               num_parallel_calls=multiprocessing_num)
 
